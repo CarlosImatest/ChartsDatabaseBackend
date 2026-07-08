1ee2978777eadccdb64ec02390d26efcb8f5ecc7
Carlos <imatest_carlos@Carloss-MacBook-Pro.local>
2026-07-08 16:00:23 -0600

    finish parsing xlsx, added user files to schema and mdoles, work in progress

---
+++ app/parsers/empty_db.py
@@ -0,0 +1,18 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[2])) # go up 3 levels: parsers -> app -> backend
+from app.db.mDB_connect_to_db import DbConnect
+
+user_input = input("enter collection name")
+
+connect = DbConnect("Charts", user_input)
+
+
+collection, client = connect.start_connection()
+
+# delete_many directly on the collection object and returns deleted count
+result = collection.delete_many({})
+
+print(f"Successfully emptied the collection. Deleted {result.deleted_count} documents.")
+
+client.close()
--- app/parsers/parser_test.py
+++ app/parsers/parser_test.py
@@ -3,6 +3,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[2])) # go up 3 levels: parsers -> app -> backend
 from app.db.mDB_connect_to_db import DbConnect
 from app.parsers.clean_xlsx import CleanXlsx
+from app.models.chart import Chart, Layer
 
 
 
@@ -11,7 +12,7 @@ class Parser_test:
         self.file_name = file_name
         #starting connection to DB
         connect = DbConnect(db_name, collection_name)
-        self.db, self.client = connect.start_connection()
+        self.collection, self.client = connect.start_connection()
 
         self.df = CleanXlsx(self.file_name)
         self.df = self.df.clean()
@@ -28,29 +29,44 @@ class Parser_test:
                     print("dict_emtpy")
                     continue
 
-                keys = list(dict_data.keys()) #getting the film's layer name
+                keys = list(dict_data.keys())
 
-                document = {
-                    "name": keys[-1],
-                    "film_type": film_type,
-                    "final_layer_value": dict_data[keys[-1]]
-                }
 
-                keys.pop() #removing the last entry from key
+                # create final layer
+                final_layer = Layer(
+                    name=keys[-1],
+                    values=dict_data[keys[-1]]
+                )
 
-                #iterating the the rest of layers
-                for i in range(len(keys)):
-                    document[f"layer{i+1}_name"] = keys[i]
-                    document[f"layer_{i+1}_value"] = dict_data[keys[i]]
 
+                # remove final layer from normal layers
+                keys.pop()
+
+
+                # create all other layers
+                layers = []
+
+                for key in keys:
+                    layer = Layer(
+                        name=key,
+                        values=dict_data[key]
+                    )
+
+                    layers.append(layer)
+
+
+                # create complete chart object
+                chart = Chart(
+                    name=final_layer.name,
+                    film_type=film_type,
+                    layers=layers,
+                    final_layer=final_layer
+                )
                 #inserting to database
-                self.db.insert_one(document)
-
+                self.collection.insert_one(chart.model_dump())
                 #clearing the variables
                 film_type = ""
-                document.clear()
                 dict_data.clear()
-
                 continue
 
             #getting the values from the dataframe
